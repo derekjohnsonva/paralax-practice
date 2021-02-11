a1dc7fa9639d393d7e264972ff5f27fc50721dd4
derekjohnsonva <derekjohnsonva@gmail.com>
2021-02-11 12:13:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import SvgIdeas from './Ideas.js';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Get Inspired</h1>
+        <p>Do the work you want to do.</p>
       </header>
+      <div className='vector'>
+        <SvgIdeas></SvgIdeas>
+      </div>
+      <footer className="App-footer">
+        <p>We have concluded</p>
+      </footer>
     </div>
   );
 }
